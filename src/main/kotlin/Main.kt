a399805ffead20b05513f176fe614aa70f7b28c9
zzzSeMzzz <zzzsemzzz@bk.ru>
2025-12-06 08:32:57 +0300

    system prompt in unput

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,30 @@
+
 import core.data.perplexety.PerMessage
 import core.network.Client
-import core.network.PerClient
 
 suspend fun main(args: Array<String>) {
-    println("Консольный чат с Perplexity для создания ТЗ мобильного приложения")
-    println("Введите exit для выхода")
-    println("Модель будет задавать вопросы, чтобы понять, что нужно создать.")
+    println("Консольный чат с Perplexity")
+    println("Введите exit для выхода, s: для задания системного промптa")
 
-    val mobilePromt = "Ты — эксперт по разработке мобильных приложений.Твоя задача составить ТЗ."
-    val mobilePromt1 = "Спроси про платформу, целевую аудиторию, функционал, дизайн. когда соберешь все 4 ответа, выдай ТЗ."
-    val mobilePromt2 = "Один вопрос один ответ"
-    val mobilePromt3 = "Ровно один вопрос за раз и ничего лишнего.Нельзя предполагать ответы пользователя"
-    val mobilePromt4 = "максимум 5-7 вопрос и после пришли готовое ТЗ"
+    val messages = mutableListOf<PerMessage>()
 
-    val messages = mutableListOf<PerMessage>().apply {
-        add(PerMessage.system("$mobilePromt $mobilePromt1 $mobilePromt3"))
-    }
-
-    //messages.add(ChatMessage.user(mobilePromt4))
 
     while (true) {
         print("Вы: ")
         val input = readlnOrNull()?.trim() ?: continue
 
-        if (input.lowercase() in listOf("exit", "выход", "quit")) {
-            println("Чат завершён.")
-            Client.close()
-            return
+        when {
+            input.lowercase() in listOf("exit", "выход", "quit") -> {
+                println("Чат завершён.")
+                Client.close()
+                return
+            }
+            input.startsWith("s:") -> {
+                messages.add(PerMessage.system(input.substring(2).trim()))
+            }
+            else -> {
+                messages.add(PerMessage.user(input))
+            }
         }
-
-        // Добавляем сообщение пользователя
-        messages.add(PerMessage.user(input))
-
-        // Отправляем всю историю
-        val answer = PerClient.askPerplexity(messages)
-        println("Agent: $answer")
-
-        // Добавляем ответ модели
-        messages.add(PerMessage.assistant(answer))
     }
 }

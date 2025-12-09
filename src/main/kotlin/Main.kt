88773e6dbf0e091022ac09dab8802791aa840634
zzzSeMzzz <zzzsemzzz@bk.ru>
2025-12-09 13:34:12 +0300

    client manager

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,11 @@ import core.network.PerClient
 import core.utils.AiClientType
 import core.utils.ClientManager
 
+
+//sonar, sonar-pro, sonar-reasoning, yandexgpt-lite
 suspend fun main(args: Array<String>) {
     val clientType = AiClientType.PERPLEXITY
-    val model = "sonar"
+    val model = "sonar"//sonar Reasoning
 
     println("Консольный чат с $clientType")
     println("Введите exit для выхода,\ns: для задания системного промптa")

180152603acd32d8704044b8eb639a66361d640e
zzzSeMzzz <zzzsemzzz@bk.ru>
2025-12-09 13:30:11 +0300

    client manager

--- src/main/kotlin/core/utils/ClientManager.kt
+++ src/main/kotlin/core/utils/ClientManager.kt
@@ -1,8 +1,75 @@
 package core.utils
 
+import core.data.perplexety.PerMessage
+import core.data.ya.ChatMessage
+import core.network.Client
+import core.network.PerClient
+
 class ClientManager {
 
-    val clients = mutableListOf<AiClient>()
+    private var yaClient: Client? = null
+    private var perClient: PerClient? = null
+
+    val messagesPer = mutableListOf<PerMessage>()/*.apply {
+        add(PerMessage.system("Ты сценарист"))
+    }*/
+    val messagesYa = mutableListOf<ChatMessage>()/*.apply {
+        add(ChatMessage.system("Ты сценарист"))
+    }*/
+
+    suspend fun ask(
+        client: AiClientType,
+        input: String,
+        isSystemPrompt: Boolean = false,
+        model: String? = null,
+        temperature: Double = 0.4
+    ): AiAnswer? {
+        return when (client) {
+            AiClientType.PERPLEXITY -> {
+                if(isSystemPrompt) {
+                    if(messagesPer.firstOrNull()?.role == "system") {
+                        messagesPer.removeAt(0)
+                    }
+                    messagesPer.add(0, PerMessage.system(input))
+                    null
+                }
+                messagesPer.add(PerMessage.user(input))
+
+                val answer = PerClient.askPerplexity(
+                    messages = messagesPer,
+                    temperature = temperature,
+                    model = model ?: "sonar",
+                )
+                messagesPer.add(PerMessage.assistant(answer.answer()))
+                answer
+            }
+            AiClientType.YANDEX_GPT -> {
+                if(isSystemPrompt) {
+                    if(messagesYa.firstOrNull()?.role == "system") {
+                        messagesYa.removeAt(0)
+                    }
+                    messagesYa.add(0, ChatMessage.system(input))
+                    return null
+                }
+
+                messagesYa.add(ChatMessage.user(input))
+
+                val answer = Client.askYaGpt(
+                    messages = messagesYa,
+                    temperature = temperature,
+                    model = model ?: "yandexgpt-lite",
+                )
+
+                messagesYa.add(ChatMessage.assistant(answer.answer()))
+                answer
+            }
+        }
+    }
+
+    fun close() {
+        yaClient?.close()
+        perClient?.close()
+    }
 }
 
 
@@ -12,16 +79,6 @@ interface AiAnswer {
     fun totalPrice(): Double
 }
 
-interface AiMessage {}
-
-interface AiClient {
-
-
-    suspend fun askAi(
-        messages: MutableList<AiMessage>,
-        temperature: Double = 0.4,
-        model: String
-    ): AiAnswer
-
-    fun close()
+enum class AiClientType {
+    PERPLEXITY, YANDEX_GPT
 }
